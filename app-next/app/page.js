30f89cf7d38e55948a2dc5737d5d53a07ecb016b
Vadim Tofan <vad.tofan@gmail.com>
2025-08-26 19:04:14 +0200

    Create Basic Landing Page

--- app-next/app/page.js
+++ app-next/app/page.js
@@ -1,5 +1,16 @@
-import styles from "./page.module.css";
+import styles from "@/app/page.module.css";
 
 export default function Home() {
-  return;
+  return (
+    <section className={styles.landing}>
+      <div>
+        <h1 className={styles.landing__title}>Get quick access to your digital health book - all your pets in one app</h1>
+        <p className={styles.landing__text}>
+          Pet Passport brings everything health-related together in one place. Our app will replace physical documents with a digital health book that gives you an overview, reminders and ensures that
+          everything you need is just a few clicks away.
+        </p>
+      </div>
+      <img src="/images/landingimage.png" alt="dog" />
+    </section>
+  );
 }
