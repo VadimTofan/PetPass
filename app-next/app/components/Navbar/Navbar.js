e2cf60ea7fff89e1869dada2131ff3ed1a182de6
Vadim Tofan <vad.tofan@gmail.com>
2025-08-31 13:47:37 +0200

    fix: Refactor Navbar component and update styles for improved usability and responsiveness

--- app-next/app/components/Navbar/Navbar.js
+++ app-next/app/components/Navbar/Navbar.js
@@ -1,39 +1,48 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import { User } from 'lucide-react';
-import styles from './Navbar.module.css';
+import Image from "next/image";
+import { User } from "lucide-react";
+import styles from "./Navbar.module.css";
+
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
-  const navItems = ['Home', 'Products', 'Blog', 'About Us', 'Contact Us'];
+  const navItems = ["Home", "Products", "Blog", "About", "Contact"];
+
+  const router = useRouter();
+
+  const handleLogo = (e) => {
+    e.preventDefault();
+    router.push("/home");
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    router.push("/signup");
+  };
 
   return (
     <nav className={styles.navbar}>
-      <div className={styles.container}>
-        <div className={styles.logo}>
-          <Image
-            className={styles['logo__img']}
-            src="/images/logo.png"
-            alt="PetPass logo"
-            width={120}
-            height={120}
-            priority
-          />
-          <span className={styles['logo__text']}>PetPass</span>
+      <div className={styles.navbar__container}>
+        <div className={styles.navbar__logo} onClick={handleLogo}>
+          <Image className={styles.navbar__logoImg} src="/images/logo.png" alt="PetPass logo" width={120} height={120} priority />
+          <span className={styles.navbar__logoText}>PetPass</span>
         </div>
 
-        <ul className={styles.navItems}>
-          {navItems.map((item, index) => (
-            <li key={index} className={styles.navItem}>
-              <a href="#" className={styles.navLink}>{item}</a>
+        <ul className={styles.navbar__items}>
+          {navItems.map((item) => (
+            <li key={item} className={styles.navbar__item}>
+              <a href={item} className={styles.navbar__link}>
+                {item}
+              </a>
             </li>
           ))}
         </ul>
 
-        <div className={styles.loginSection}>
-          <button className={styles.loginButton}>
+        <div className={styles.navbar__auth} onClick={handleLogin}>
+          <button className={styles.navbar__loginButton}>
             <User size={20} />
-            <span>Login</span>
+            <span className={styles.navbar__loginText}>Login</span>
           </button>
         </div>
       </div>
