6b7964ee99fc7157265656543415e0fa1095ac6a
sathic <sathichowdhury2017@gmail.com>
2025-09-01 14:37:06 +0200

    Update navbar: responsive icons, text under icons, thin layout

--- app-next/app/components/Navbar/Navbar.js
+++ app-next/app/components/Navbar/Navbar.js
@@ -7,7 +7,11 @@ import styles from "./Navbar.module.css";
 import { useRouter } from "next/navigation";
 
 const Navbar = () => {
-  const navItems = ["Home", "Products", "Blog", "About", "Contact"];
+  const navItems = [
+    { name: "Home", icon: "/icons/home-icon.png", path: "/home" },
+    { name: "About Us", icon: "/icons/about-icon.png", path: "/about" },
+    { name: "Contact Us", icon: "/icons/contact-icon.png", path: "/contact" },
+  ];
 
   const router = useRouter();
 
@@ -22,31 +26,47 @@ const Navbar = () => {
   };
 
   return (
-    <nav className={styles.navbar}>
+    <div className={styles.navbar__top}>
       <div className={styles.navbar__container}>
         <div className={styles.navbar__logo} onClick={handleLogo}>
-          <Image className={styles.navbar__logoImg} src="/images/logo.png" alt="PetPass logo" width={120} height={120} priority />
+          <Image
+            className={styles.navbar__logoImg}
+            src="/images/logo.png"
+            alt="PetPass logo"
+            width={120}
+            height={120}
+            priority
+          />
           <span className={styles.navbar__logoText}>PetPass</span>
         </div>
-
-        <ul className={styles.navbar__items}>
-          {navItems.map((item) => (
-            <li key={item} className={styles.navbar__item}>
-              <a href={item} className={styles.navbar__link}>
-                {item}
-              </a>
-            </li>
-          ))}
-        </ul>
-
-        <div className={styles.navbar__auth} onClick={handleLogin}>
-          <button className={styles.navbar__loginButton}>
-            <User size={20} />
-            <span className={styles.navbar__loginText}>Login</span>
-          </button>
-        </div>
       </div>
-    </nav>
+      <nav className={styles.navbar}>
+        <div className={styles.navbar__container}>
+          <ul className={styles.navbar__items}>
+            {navItems.map((item) => (
+              <li key={item.name} className={styles.navbar__item}>
+                <a href={item.path} className={styles.navbar__link}>
+                  <Image
+                    src={item.icon}
+                    alt={item.name}
+                    width={24}
+                    height={24}
+                  />
+                  <span className={styles.navbar__linkText}>{item.name}</span>
+                </a>
+              </li>
+            ))}
+          </ul>
+
+          <div className={styles.navbar__auth} onClick={handleLogin}>
+            <button className={styles.navbar__loginButton}>
+              <User size={20} />
+              <span className={styles.navbar__loginText}>Login</span>
+            </button>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 };
 
