d91693b2b6d00f3aff8ab6eb139bc70c323d5148
Vadim Tofan <vad.tofan@gmail.com>
2025-09-07 14:51:46 +0200

    fix images

--- app-next/next.config.mjs
+++ app-next/next.config.mjs
@@ -2,8 +2,21 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "http", hostname: "localhost", port: "8000", pathname: "/uploads/**" },
-      { protocol: "https", hostname: "**" }
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "http",
+        hostname: "petpass-fulf.onrender.com",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
   },
 };
