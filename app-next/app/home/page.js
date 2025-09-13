6a3953af30d664cc4989d264de40cb4022108334
Vadim Tofan <vad.tofan@gmail.com>
2025-09-13 14:32:46 +0200

    - Fix navigation

--- app-next/app/home/page.js
+++ app-next/app/home/page.js
@@ -1,6 +1,6 @@
 import Hero from "@/app/home/hero/Hero";
 import Category from "@/app/home/category/Category";
-import Features from "@/app/home/promotion/Features";
+import Features from "@/app/home/features/Features";
 
 export default function Main() {
   return (
