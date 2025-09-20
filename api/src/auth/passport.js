37aff4a1e0f3a396a28507a3fa46e585e7b8a2fb
Vadim Tofan <vad.tofan@gmail.com>
2025-09-20 14:09:56 +0200

    refactor: clean up whitespace and improve code formatting in passport.js

--- api/src/auth/passport.js
+++ api/src/auth/passport.js
@@ -1,11 +1,10 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import * as db from "../database/users.js"; 
+import * as db from "../database/users.js";
 
 import dotenv from "dotenv";
 
 dotenv.config();
-
 
 passport.use(
   new GoogleStrategy(
@@ -16,7 +15,6 @@ passport.use(
     },
     async (_accessToken, _refreshToken, profile, done) => {
       try {
-      
         const googleId = profile.id;
         const email = profile.emails?.[0]?.value;
         const full_name = profile.displayName;
@@ -24,14 +22,14 @@ passport.use(
 
         let user = await db.getUserByEmail(email);
         if (!user) {
-          user = await db.createUser({
+          user = await db.addUser({
             email,
             full_name,
             googleid: googleId,
             photo,
           });
         }
-      
+
         return done(null, {
           id: user.id,
           email,
@@ -47,11 +45,9 @@ passport.use(
   )
 );
 
-
 passport.serializeUser((user, done) => {
-  done(null, user); 
+  done(null, user);
 });
-
 
 passport.deserializeUser((user, done) => {
   done(null, user);
