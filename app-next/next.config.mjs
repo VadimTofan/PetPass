44203d547f06e17b4a17846c2d689510e845d1de
Vadim Tofan <vad.tofan@gmail.com>
2025-09-22 01:24:56 +0200

    fix images

--- app-next/next.config.mjs
+++ app-next/next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: [
+      "lh3.googleusercontent.com", // Google profile photos
+      "petpass-fulf.onrender.com", // your backend
+    ],
+  },
+
   async rewrites() {
     return [
       { source: "/api/:path*", destination: "https://petpass-fulf.onrender.com/api/:path*" },
